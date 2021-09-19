30bbf6689301506348fbce7c219165fa02834825
Cesar Entzana Ramírez <cesarbatzu12@nube.unadmexico.mx>
2021-09-19 04:15:22 -0500

    Se agrega la class de reglas para definir una variable en JAVA

---
+++ ReglasDefincionVariables/ReglasDefincionVariables/ReglasDefincionVariables.java
@@ -0,0 +1,10 @@
+public class ReglasDefincionVariables {
+    public static void main(String[] args) {
+        var variable1 = 1;
+        var variable2 = 2;
+        var variable3 = 3;
+
+        // var áVariable = 10;  No se puede utilizar asentos 
+        // var &Variable = 20;  No se permite utilizar caracteres especiales 
+    }
+}
