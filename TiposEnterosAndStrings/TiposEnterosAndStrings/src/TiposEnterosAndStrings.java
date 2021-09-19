024e1a0cc052ea11132830de0fad94746b648fda
Cesar Entzana Ramírez <cesarbatzu12@nube.unadmexico.mx>
2021-09-19 03:39:03 -0500

    Agregada class con 2 ejemplos de enteros y dos String

---
+++ TiposEnterosAndStrings/TiposEnterosAndStrings/src/TiposEnterosAndStrings.java
@@ -0,0 +1,20 @@
+public class TiposEnterosAndStrings {
+    
+    public static void main(String[] args)  {
+    // Variable entera 
+    int variableEntera = 10;
+    System.out.println(variableEntera+"\n");
+
+    // Variable entera 2 
+    int variableEntera2 = 20 ;
+    System.out.println(variableEntera2+"\n");
+
+    // Cadena de caracteres 
+    String cadena1 = "Cadena uno ";
+    System.out.println(cadena1+"\n");
+
+    // Cadena de caracteres 2 
+     String cadena2 = "Cadena dos ";
+     System.out.println(cadena2+"\n");
+}
+}
