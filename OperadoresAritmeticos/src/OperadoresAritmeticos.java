4be64965075ef9dbf57b5ac679095f3f5f60b185
Cesar Entzana Ramírez <cesarbatzu12@nube.unadmexico.mx>
2021-09-20 04:35:50 -0500

    Agregado operadores aritmeticos con operaciones basicas {suma, resta, disión , multiplicación y módulo}

---
+++ OperadoresAritmeticos/src/OperadoresAritmeticos.java
@@ -0,0 +1,30 @@
+public class OperadoresAritmeticos {
+    
+
+    public static void main(String[] args) {
+       // Opradores aritéticos
+       int a=10;
+       int b=20;
+
+       // Suma 
+       var resultado = a+b;
+       System.out.println("Suma a + b = "+resultado);
+       
+       // Resta
+       resultado = a-b;
+       System.out.println("Resta a - b = "+resultado);
+       
+       // Multiplicación 
+       resultado = a*b;
+       System.out.println("Multiplicación a * b = "+resultado);
+       
+       // División 
+       var  div = (float)a/b;
+       System.out.println("División  a / b = "+div);
+       
+       // Módulo
+        resultado = ( a % b);
+       System.out.println("Módulo a % b = "+resultado);
+       
+    }
+}
