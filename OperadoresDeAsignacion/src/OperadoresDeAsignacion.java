a6769f4599132fd3f593d3855e059f09346ead2d
Cesar Entzana Ramírez <cesarbatzu12@nube.unadmexico.mx>
2021-09-21 23:32:16 -0500

    se agrega el uso de operadores de asignación

---
+++ OperadoresDeAsignacion/src/OperadoresDeAsignacion.java
@@ -0,0 +1,21 @@
+public class OperadoresDeAsignacion {
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 2 ;
+        int c =  a +5 -b ;
+
+System.out.println(c);
+
+ a += 1;  // La variable se incrementa el valor en uno (a + 1)
+
+ System.out.println("a = "+a);
+
+ a += 3; // la variable se incrementa con un valor de ma stres (a + 3 )
+
+ System.out.println("a = "+a);
+
+ a -= 2 ; // La variable se decrementa en un valor de -2 
+
+ System.out.println("a = "+a);
+    }
+}
