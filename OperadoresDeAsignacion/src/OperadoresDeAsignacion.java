e7b4beac1db6f50e2a34f087e8778b53fffb507a
Cesar Entzana Ramírez <cesarbatzu12@nube.unadmexico.mx>
2021-09-26 04:31:08 -0500

    new commit

--- OperadoresDeAsignacion/src/OperadoresDeAsignacion.java
+++ OperadoresDeAsignacion/src/OperadoresDeAsignacion.java
@@ -1,10 +1,19 @@
 public class OperadoresDeAsignacion {
     public static void main(String[] args) {
+
+        /*
+        Operadores de asignación se utilizan mediante el uso de variables y en ellas se 
+        asignan valores procedentes de la evaluación de los valores que s eencuentreen 
+        en el lado derecho y una vez evaluados se asignan a la variable referida en la parte 
+        izquierda mediante el operador de asignación (=)
+        */
         int a = 3;
         int b = 2 ;
         int c =  a +5 -b ;
 
 System.out.println(c);
+
+// Operadores de asignación compuestos permite simplificar la sintaxis del mismo sin tener que definir tanto código
 
  a += 1;  // La variable se incrementa el valor en uno (a + 1)
 
