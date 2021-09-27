5eb42ade8cd97dbd66f6bdd71eba84c069be3def
Cesar Entzana Ramírez <cesarbatzu12@nube.unadmexico.mx>
2021-09-27 03:41:53 -0500

    Actualización de Operadores Unarios

--- OperadoresUnarios/src/OperadoresUnarios.java
+++ OperadoresUnarios/src/OperadoresUnarios.java
@@ -20,14 +20,36 @@ public class OperadoresUnarios {
         System.out.println("c = " + c);
         System.out.println("d = " + d);
 
+        // Preincrmeento con operador antes de la variable
 
-        // Preincrmeento 
+        var e = 3;
+        var f = ++e; // s eincrmenta la variable y enseguida se utiliza su valor
 
-        var e = 3 ;
-        var f = ++e; // s eincrmenta la variable y enseguida se utiliza su valor 
+        System.out.println("e = " + e);
+        System.out.println("f = " + f);
+        var g = 5;
+        var h = g++; // Es asiganado el valor por defecto de la variable y en seguida se incrmenta
 
-        System.out.println("e = "+e);
-        System.out.println("f = "+f);
+        System.out.println("g = " + g); // Contiene el valor inicial de la variable sin incremento
+        System.out.println("h = " + h); // Contiene el valor de la variable g con un incremento en una unidad
+
+        // Decremento
+
+        var i = 2;
+        var j = --i; // La variable tiene un decremento en una unidad y el valor se asigna a la
+                     // variable
+
+        System.out.println("i = " + i);
+        System.out.println("j = " + j);
+
+        // Posdecremento
+
+        var k = 4;
+        var l = k--; // En primer lugar se asigna el valor a la variable declarada "l" seguido del
+                     // decrmento en una unidad
+
+        System.out.println("k = " + k);
+        System.out.println("l = " + l);
 
     }
 }
